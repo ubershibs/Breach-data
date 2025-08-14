e69eb8905e125e3d1626e50ba0f63337036d2554
Luke Walker <luke.walker@gmail.com>
2025-08-14 10:12:39 -0300

    Working bulk-generate PDF script

--- generate-pdfs.js
+++ generate-pdfs.js
@@ -3,23 +3,24 @@ const fs = require('fs');
 const path = require('path');
 const csv = require('csv-parser');
 
-const studentsToProcess = [4511157, 3541153]; // Example student numbers to process
+const studentsToProcess = []; // Example student numbers to process
 
+/*
 // Read the student numbers from the same CSV file
 fs.createReadStream(path.join(__dirname, 'src', 'data', 'students.csv'))
   .pipe(csv())
   .on('data', (row) => {
     // Assuming the column in your CSV is named 'Student_Number'
     if (row.Student_Number) {
-      studentsToProcess.push(row.Student_Number);
+      studentsToProcessRows.push(row.Student_Number);
     }
   })
   .on('end', async () => {
     console.log(`Found ${studentsToProcess.length} students to process.`);
     await generatePdfs();
   });
-
-async function generatePdfs() {
+*/
+async function generatePdfs(studentsToProcess) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
@@ -28,10 +29,10 @@ async function generatePdfs() {
   if (!fs.existsSync(pdfDir)){
       fs.mkdirSync(pdfDir);
   }
-
+   
   // You can use a slice for testing, e.g., studentsToProcess.slice(0, 5)
   for (const studentNumber of studentsToProcess) {
-    const url = `http://localhost:3000/studentnumber?studentnumber=${studentNumber}`;
+    const url = `https://localhost:3443/studentnumber?studentnumber=${studentNumber}`;
     console.log(`Navigating to ${url}`);
     
     await page.goto(url, { waitUntil: 'networkidle0' });
@@ -51,3 +52,6 @@ async function generatePdfs() {
   await browser.close();
   console.log('All PDFs have been generated.');
 } 
+
+
+generatePdfs(studentsToProcess);
